0bb958fb8a75243ed17c0486bba80790d5ad2af5
Tho Nguyen <nguyenhoangtrungtho9130@gmail.com>
2021-10-08 09:54:32 +0700

    connect app with wallet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
+
+function getLibrary(provider) {
+  return new Web3(provider);
+}
 
 ReactDOM.render(
-  <>
+  <Web3ReactProvider getLibrary={getLibrary}>
     <App />
-  </>,
-  document.getElementById('root')
+  </Web3ReactProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,38 @@
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { useWeb3React } from "@web3-react/core";
+
 const Home = () => {
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
+
+  const injected = new InjectedConnector({
+    supportedChainIds: [1, 3, 4, 5, 42],
+  });
+
+  async function connect() {
+    try {
+      await activate(injected);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async function disconnect() {
+    try {
+      deactivate();
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
   return (
     <div className="home-page ">
       {/* <Container className=""> */}
       <div className="colection">
-        <span>Go to your collection</span>
+        <span onClick={connect}>Go to your collection</span>
+        {/* {active ? 'a' : 'b '} */}
       </div>
+
       {/* </Container> */}
     </div>
   );
