b809a08815acb430084078d809e6b6af859e1966
reedad <reedad@gmail.com>
2021-11-14 14:39:22 +0700

    add log

--- src/pages/account/accountInfo.js
+++ src/pages/account/accountInfo.js
@@ -59,6 +59,11 @@ const AccountInfo = () => {
 
     // I have hardcoded my secondary wallet address here. You can take this address either from user input or your DB or wherever
     var recieverWallet = new web3.PublicKey(transactionInfo?.toWallet || "");
+    console.log("recieverWallet : ", recieverWallet);
+    //   {
+    //     "_bn": "6b4a4342f6b534d47131e3bbb42b342549b6a67e5ce0e0439c8fb9a9aa359d44"
+    // }
+
 
     // Airdrop some SOL to the sender's wallet, so that it can handle the txn fee
     var airdropSignature = await connection.requestAirdrop(
@@ -66,10 +71,20 @@ const AccountInfo = () => {
       web3.LAMPORTS_PER_SOL,
     );
 
+    console.log("airdropSignature : ", airdropSignature);
+    // 5LFYwYbWWZc9Wm4qWDoTTd9B7f2uH2sVKa2XgpPEfwLJgMFys99QvDfDBq3uAJ1S9caiiPwgzaens2kw7AfcpMCt
     // Confirming that the airdrop went through
-    await connection.confirmTransaction(airdropSignature);
-    console.log("Airdropped");
-
+    const Airdropped =  await connection.confirmTransaction(airdropSignature);
+    console.log("Airdropped", Airdropped);
+  //   {
+  //     "context": {
+  //         "slot": 94131912
+  //     },
+  //     "value": {
+  //         "err": null
+  //     }
+  // }
+      // khúc này nó khong hỏi 
     var transaction = new web3.Transaction().add(
       web3.SystemProgram.transfer({
         fromPubkey: provider.publicKey,
@@ -77,6 +92,130 @@ const AccountInfo = () => {
         lamports: (transactionInfo?.quantity) * web3.LAMPORTS_PER_SOL //Investing 1 SOL. Remember 1 Lamport = 10^-9 SOL.
       }),
     );
+
+    console.log("transaction", transaction);
+  //   {
+  //     "signatures": [
+  //         {
+  //             "signature": {
+  //                 "type": "Buffer",
+  //                 "data": [
+  //                     23,
+  //                     133,
+  //                     151,
+  //                     69,
+  //                     135,
+  //                     179,
+  //                     88,
+  //                     110,
+  //                     170,
+  //                     87,
+  //                     185,
+  //                     165,
+  //                     134,
+  //                     164,
+  //                     8,
+  //                     71,
+  //                     4,
+  //                     18,
+  //                     96,
+  //                     243,
+  //                     131,
+  //                     64,
+  //                     221,
+  //                     58,
+  //                     188,
+  //                     195,
+  //                     239,
+  //                     107,
+  //                     34,
+  //                     33,
+  //                     211,
+  //                     224,
+  //                     170,
+  //                     243,
+  //                     99,
+  //                     66,
+  //                     169,
+  //                     250,
+  //                     168,
+  //                     43,
+  //                     40,
+  //                     60,
+  //                     216,
+  //                     220,
+  //                     33,
+  //                     4,
+  //                     219,
+  //                     167,
+  //                     98,
+  //                     80,
+  //                     203,
+  //                     249,
+  //                     195,
+  //                     195,
+  //                     235,
+  //                     135,
+  //                     151,
+  //                     239,
+  //                     111,
+  //                     62,
+  //                     100,
+  //                     138,
+  //                     118,
+  //                     8
+  //                 ]
+  //             },
+  //             "publicKey": {
+  //                 "_bn": "f14407b04d14ccd5b37687fc036a2efb0c0350e6972b070d3e4529a79885a69f"
+  //             }
+  //         }
+  //     ],
+  //     "feePayer": {
+  //         "_bn": "f14407b04d14ccd5b37687fc036a2efb0c0350e6972b070d3e4529a79885a69f"
+  //     },
+  //     "instructions": [
+  //         {
+  //             "keys": [
+  //                 {
+  //                     "pubkey": {
+  //                         "_bn": "f14407b04d14ccd5b37687fc036a2efb0c0350e6972b070d3e4529a79885a69f"
+  //                     },
+  //                     "isSigner": true,
+  //                     "isWritable": true
+  //                 },
+  //                 {
+  //                     "pubkey": {
+  //                         "_bn": "6b4a4342f6b534d47131e3bbb42b342549b6a67e5ce0e0439c8fb9a9aa359d44"
+  //                     },
+  //                     "isSigner": false,
+  //                     "isWritable": true
+  //                 }
+  //             ],
+  //             "programId": {
+  //                 "_bn": "00"
+  //             },
+  //             "data": {
+  //                 "type": "Buffer",
+  //                 "data": [
+  //                     2,
+  //                     0,
+  //                     0,
+  //                     0,
+  //                     0,
+  //                     225,
+  //                     245,
+  //                     5,
+  //                     0,
+  //                     0,
+  //                     0,
+  //                     0
+  //                 ]
+  //             }
+  //         }
+  //     ],
+  //     "recentBlockhash": "BXCeGFpP1dxZJaZxFXbL6d1koJynMMtn56HfY1JWXCcW"
+  // }
 
     // Setting the variables for the transaction
     transaction.feePayer = await provider.publicKey;
@@ -90,13 +229,151 @@ const AccountInfo = () => {
 
     // Request creator to sign the transaction (allow the transaction)
     let signed = await provider.signTransaction(transaction);
+    // khúc này nó mới yều cầu confirm 
+    console.log("signed", signed);
+  //   {
+  //     "signatures": [
+  //         {
+  //             "signature": {
+  //                 "type": "Buffer",
+  //                 "data": [
+  //                     23,
+  //                     133,
+  //                     151,
+  //                     69,
+  //                     135,
+  //                     179,
+  //                     88,
+  //                     110,
+  //                     170,
+  //                     87,
+  //                     185,
+  //                     165,
+  //                     134,
+  //                     164,
+  //                     8,
+  //                     71,
+  //                     4,
+  //                     18,
+  //                     96,
+  //                     243,
+  //                     131,
+  //                     64,
+  //                     221,
+  //                     58,
+  //                     188,
+  //                     195,
+  //                     239,
+  //                     107,
+  //                     34,
+  //                     33,
+  //                     211,
+  //                     224,
+  //                     170,
+  //                     243,
+  //                     99,
+  //                     66,
+  //                     169,
+  //                     250,
+  //                     168,
+  //                     43,
+  //                     40,
+  //                     60,
+  //                     216,
+  //                     220,
+  //                     33,
+  //                     4,
+  //                     219,
+  //                     167,
+  //                     98,
+  //                     80,
+  //                     203,
+  //                     249,
+  //                     195,
+  //                     195,
+  //                     235,
+  //                     135,
+  //                     151,
+  //                     239,
+  //                     111,
+  //                     62,
+  //                     100,
+  //                     138,
+  //                     118,
+  //                     8
+  //                 ]
+  //             },
+  //             "publicKey": {
+  //                 "_bn": "f14407b04d14ccd5b37687fc036a2efb0c0350e6972b070d3e4529a79885a69f"
+  //             }
+  //         }
+  //     ],
+  //     "feePayer": {
+  //         "_bn": "f14407b04d14ccd5b37687fc036a2efb0c0350e6972b070d3e4529a79885a69f"
+  //     },
+  //     "instructions": [
+  //         {
+  //             "keys": [
+  //                 {
+  //                     "pubkey": {
+  //                         "_bn": "f14407b04d14ccd5b37687fc036a2efb0c0350e6972b070d3e4529a79885a69f"
+  //                     },
+  //                     "isSigner": true,
+  //                     "isWritable": true
+  //                 },
+  //                 {
+  //                     "pubkey": {
+  //                         "_bn": "6b4a4342f6b534d47131e3bbb42b342549b6a67e5ce0e0439c8fb9a9aa359d44"
+  //                     },
+  //                     "isSigner": false,
+  //                     "isWritable": true
+  //                 }
+  //             ],
+  //             "programId": {
+  //                 "_bn": "00"
+  //             },
+  //             "data": {
+  //                 "type": "Buffer",
+  //                 "data": [
+  //                     2,
+  //                     0,
+  //                     0,
+  //                     0,
+  //                     0,
+  //                     225,
+  //                     245,
+  //                     5,
+  //                     0,
+  //                     0,
+  //                     0,
+  //                     0
+  //                 ]
+  //             }
+  //         }
+  //     ],
+  //     "recentBlockhash": "BXCeGFpP1dxZJaZxFXbL6d1koJynMMtn56HfY1JWXCcW"
+  // }
+
     // The signature is generated
     let signature = await connection.sendRawTransaction(signed.serialize());
+    console.log("signature", signature);
+    // 5LFYwYbWWZc9Wm4qWDoTTd9B7f2uH2sVKa2XgpPEfwLJgMFys99QvDfDBq3uAJ1S9caiiPwgzaens2kw7AfcpMCt
     // Confirm whether the transaction went through or not
-    await connection.confirmTransaction(signature);
+    const confirmTransac = await connection.confirmTransaction(signature);
+
+    console.log("confirmTransac", confirmTransac);
+  //   {
+  //     "context": {
+  //         "slot": 94131979
+  //     },
+  //     "value": {
+  //         "err": null
+  //     }
+  // }
 
     //Signature chhap diya idhar
     console.log("Signature: ", signature);
+    // UH1fn2gUcmSmSe3hkRZFtibTRUVth3M6f9ApnVSxvhqomwtfz7L87vT9BRkfRuxDKYxoVEqfdEY9YGY8mUCrpSB
     setIsComplete(true)
   }
 
@@ -116,7 +393,7 @@ const mintingTest = async () => {
 
   //This fromWallet is your minting wallet, that will actually mint the tokens
   var fromWallet = web3.Keypair.generate();
-
+  console.log("fromWallet : ", fromWallet);
   // Associate the mintRequester with this wallet's publicKey and privateKey
   // This is basically the credentials that the mintRequester (creator) would require whenever they want to mint some more tokens
  // Testing the parameters of the minting wallet
@@ -129,10 +406,11 @@ const mintingTest = async () => {
     fromWallet.publicKey,
     web3.LAMPORTS_PER_SOL,
   );
+  console.log("fromAirDropSignature : ", fromAirDropSignature);
 
-  await connection.confirmTransaction(fromAirDropSignature);
+  const aaa = await connection.confirmTransaction(fromAirDropSignature);
   console.log("Airdropped (transferred) 1 SOL to the fromWallet to carry out minting operations");
-
+  console.log("confirmTransaction : ", aaa);
   // This createMint function returns a Promise <Token>
   let mint = await splToken.Token.createMint(
     connection,
@@ -143,11 +421,15 @@ const mintingTest = async () => {
     splToken.TOKEN_PROGRAM_ID,
   );
 
+  console.log("mint : ", mint);
   // getting or creating (if doens't exist) the token address in the fromWallet address
   // fromTokenAccount is essentially the account *inside* the fromWallet that will be able to handle the              new token that we just minted
   let fromTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
     fromWallet.publicKey,
   );
+
+  
+  console.log("fromTokenAccount : ", fromTokenAccount);
 
   // getting or creating (if doens't exist) the token address in the toWallet address
   // toWallet is the creator: the og mintRequester
@@ -156,21 +438,27 @@ const mintingTest = async () => {
     mintRequester,
   );
 
+  console.log("toTokenAccount : ", toTokenAccount);
+
   // // Minting 1 token
-  await mint.mintTo(
+  const bbb = await mint.mintTo(
     fromTokenAccount.address,
     fromWallet.publicKey,
     [],
     1000000 // 1 followed by decimals number of 0s // You'll ask the creator ki how many decimals he wants in his token. If he says 4, then 1 token will be represented as 10000
   );
 
-  await mint.setAuthority(
+  console.log("mintTo : ", bbb);
+
+  const aabb = await mint.setAuthority(
     mint.publicKey,
     null,
     "MintTokens",
     fromWallet.publicKey,
     []
   )
+
+  console.log("aabb : ", aabb);
 
   console.log("Initial mint successful");
 
@@ -187,12 +475,16 @@ const mintingTest = async () => {
     ),
   );
 
+  console.log("transaction : ", transaction);
+
   var signature = await web3.sendAndConfirmTransaction(
     connection,
     transaction,
     [fromWallet],
     {commitment: 'confirmed'},
   );
+
+  console.log("signature : ", signature);
 
   const creatorTokenAddress = mint.publicKey;
   const creatorTokenAddressString = mint.publicKey.toString();
